7bd4a33db4c906efb97129bcaae47eb9d84316e6
cosmicasymmetry <cosmicasymmetry@github.com>
2018-04-24 05:50:49 +0530

    Adding Workshop page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ router.get('/', function(req, res) {
 	res.render('index');
 });
 
+router.get('/workshops', function(req, res) {
+	res.render('workshops');
+});
+
 router.get('/login', function(req, res) {
 	res.render('login');
 });
