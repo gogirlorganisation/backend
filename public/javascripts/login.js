c561b5cb9ad07c1827d9b9e2930954b58db51926
Naman Sood <mail@nsood.in>
2018-04-28 01:16:42 +0530

    Eve changes

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -19,10 +19,9 @@ $('form').on('submit', function(e) {
 		success: function(data) {
 			if(data.message == 'success')
 				location.href = '/dashboard';
-			else alert(data.message);
+			else alert('Invalid details - try again.');
 		},
 		error: function(xhr, err) {
-			alert('see console for error');
 			console.log(err);     
 		}
 	});
