1e20739fb8a7235db42e84b6b40902f345d81738
Naman Sood <mail@nsood.in>
2018-05-21 19:02:03 +0530

    Team images and favicon fix

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(passport.session());
 mongoose.connect(auth.url);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
