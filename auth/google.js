cb1f89f4cb6a4d7075eb803ddce51897202d49c0
Naman Sood <mail@nsood.in>
2018-07-26 21:09:11 +0530

    Google sign in: update API keys

--- auth/google.js
+++ auth/google.js
@@ -2,22 +2,24 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var User = require('./models/User');
 var bCrypt = require('bcrypt-nodejs');
 
+var callbackURL = 'http://localhost:3000/users/google/callback';
+
+if(process.env.NODE_ENV === 'production')
+	callbackURL = 'http://learn.thegirlcode.co/users/google/callback';
+
 module.exports = function(passport) {
 	passport.use('google', new GoogleStrategy({
-		clientID: '263339810522-30u8oe75vteqmq50kmmj2rqbcerf6aok.apps.googleusercontent.com',
-		clientSecret: 'wxunTktEBjLN9aYk1nZg-W3Q',
-		callbackURL: 'http://localhost:3000/users/google/callback'
+		clientID: '***REMOVED***',
+		clientSecret: '***REMOVED***',
+		callbackURL: callbackURL
 	}, function(accessToken, refreshToken, profile, done) {
 		var email = profile.emails[0].value;
 		User.findOne({
 			email: email
 		}, function(err, user) {
-			console.log('============IN GOOGLE AUTHENTICATION');
 			if(err) throw err;
 
 			else if(user) {
-				console.log('===========FOUND USER');
-				console.log(user);
 				return done(null, user, { message: 'success' });
 			}
 
